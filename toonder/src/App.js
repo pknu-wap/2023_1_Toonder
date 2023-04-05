ef1dbf13ac100e2bd8e32aa25893222e2e278e54
kitewatermelon <80404308+kitewatermelon@users.noreply.github.com>
2023-04-05 12:56:28 +0900

    fix :배경화면 짤림 현상 제거/파일을 컴포넌트 단위로 나눔

--- toonder/src/App.js
+++ toonder/src/App.js
@@ -1,10 +1,12 @@
-import Findid from "./findid";
+import Findid from "./component/findid";
+import { BrowserRouter, Route , Switch } from 'react-router-dom';
 
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <Findid/>
-    </div>
+      <div className="App">
+        <Findid/>
+      </div>
   );
 }
 
