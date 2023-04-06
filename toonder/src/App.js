a8c7d925b06eb28eedf9c4c7d721eb9ff16c874b
이승엽 <dltmdduq8484@gmail.com>
2023-04-07 03:21:40 +0900

    이거머지해주면됨걍 별거아니고 ID_background.js를 로그인페이지랑 찾기페이지가 공유하는데 서로각자쓸때 다르게 쓸수잇는 텍스트를 프롭스로 전달햇음

--- toonder/src/App.js
+++ toonder/src/App.js
@@ -1,16 +1,13 @@
-
 import Findid from './component/findid';
 import Login from './component/login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import FindidAfter from "./component/findid_after";
+import FindidAfter from './component/findid_after';
 
 import React from 'react';
 function App() {
   return (
     <div className="App">
-
       <Findid />
-
     </div>
   );
 }
