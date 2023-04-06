ef3248ea8bed25fe9b09556b1b3c8e09b9c0f5a0
이승엽 <dltmdduq8484@gmail.com>
2023-04-07 03:21:40 +0900

    이거머지해주면됨걍 별거아니고 ID_background.js를 로그인페이지랑 찾기페이지가 공유하는데 서로각자쓸때 다르게 쓸수잇는 텍스트를 프롭스로 전달햇음

--- toonder/src/component/findid.js
+++ toonder/src/component/findid.js
@@ -1,11 +1,9 @@
-
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 import styles from '../style.css';
 import IDBackground from './ID_background';
 
-import Findid_after from "./findid_after";
-
+import Findid_after from './findid_after';
 
 function Findid() {
   const [isFindid, setIsFindid] = useState(true);
@@ -15,21 +13,20 @@ function Findid() {
   }
 
   return (
-
-    <IDBackground>
-
+    <IDBackground text="Find">
       {isFindid ? (
         <div className="findid_body">
           <form>
             <input id="enter_name" type="text" placeholder="Enter your name" />
             <input id="enter_id" type="text" placeholder="Enter your ID" />
-            <button type="button" onClick={switchToFindidAfter}><strong>Find</strong></button>
+            <button type="button" onClick={switchToFindidAfter}>
+              <strong>Find</strong>
+            </button>
           </form>
         </div>
       ) : (
         <Findid_after />
       )}
-
     </IDBackground>
   );
 }
