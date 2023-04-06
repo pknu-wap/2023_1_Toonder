fbb193f39e17266aa20af8f1ab8ea35711dcc429
kitewatermelon <80404308+kitewatermelon@users.noreply.github.com>
2023-04-06 16:21:05 +0900

    fix: some folder and function's name has been changed

---
+++ toonder/src/component/findid_after.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from '../style.css';
+import IDBackground from "./ID_background";
+
+
+function Findid_after() {
+  return (
+    <IDBackground>
+      <div className="findid_after">
+
+      </div>
+    </IDBackground>
+  );
+}
+
+export default Findid_after;
