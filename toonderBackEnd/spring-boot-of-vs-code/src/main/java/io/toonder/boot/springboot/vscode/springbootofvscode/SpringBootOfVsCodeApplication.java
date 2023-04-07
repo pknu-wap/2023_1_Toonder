87775981d8c28d9ae5159f959ace577740b7efe3
JangHongJoon <wkdghdwns19969@gmail.com>
2023-04-07 16:42:28 +0900

    create spring project

---
+++ toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/SpringBootOfVsCodeApplication.java
@@ -0,0 +1,13 @@
+package io.toonder.boot.springboot.vscode.springbootofvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOfVsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOfVsCodeApplication.class, args);
+	}
+
+}
