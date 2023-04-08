a9feede12d62166e2a6cd3c350c35c664b0d32f8
kitewatermelon <80404308+kitewatermelon@users.noreply.github.com>
2023-04-09 02:13:41 +0900

    fix: joinFindBtn.js 파일을 로그인.js파일로 수정하고 로그인에서 아이디 찾기를 누르면 화면 전환하는 것 까지 만들었으나 아직 많은 버그가 있습니다.

--- toonder/src/component/login.js
+++ toonder/src/component/login.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../style.css';
 import JoinFindBtn from './joinFindBtn';
 import IDBackground from './ID_background';
+import Findid from './findid';
+
 
 function Login() {
+
+  const [isFindid, setIsFindid] = useState(true);
+
+  function switchToFindid() {
+    setIsFindid(false);
+  }
+
   return (
-    <div>
-      <IDBackground text="Login">
+  <div>
+    {isFindid ? (
+        <IDBackground text="Login">
         <div className="findid_body">
           <form>
             <input id="enter_name" type="text" placeholder="Enter your ID" />
@@ -19,9 +29,14 @@ function Login() {
               <strong>Login</strong>
             </button>
           </form>
+          <form>
+            <button onClick={switchToFindid}>아이디 찾기</button> <button>비밀번호 찾기</button> <button>회원가입</button>
+          </form>
         </div>
-      </IDBackground>
-      <JoinFindBtn />
+        </IDBackground>
+        ) : (
+        <Findid />
+      )}
     </div>
   );
 }
