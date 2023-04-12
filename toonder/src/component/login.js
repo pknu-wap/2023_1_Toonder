9b4f97709a58c581cb6b00680fca4dbf58772506
Lee SeungYeop <dltmdduq8484@gmail.com>
2023-04-13 01:02:52 +0900

    CSS Module 방식으로 login.js 컴포넌트를 스타일링하고 style.css에 안쓰는 값을 삭제

--- toonder/src/component/login.js
+++ toonder/src/component/login.js
@@ -1,37 +1,36 @@
 import React, { useState } from 'react';
-import styles from '../style.css';
+import styles from './login.module.css'; //CSS Module 이라는 기술을 사용하면, CSS 클래스가 중첩되는 것을 완벽히 방지할 수 있음 C
 import IDBackground from './ID_background';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
   let navigate = useNavigate();
   return (
+    //클래스를 설정할땐 styles객체안에 있는 값을 참조하는 방식
     <div>
       <IDBackground text="Login">
-        <div className="findid_body">
+        <div className={styles.Login}>
           <form>
-            <input id="enter_name" type="text" placeholder="Enter your ID" />
-            <input
-              id="enter_id"
-              type="text"
-              placeholder="Enter your password"
-            />
+            <input type="text" placeholder="Enter your ID" />
+            <input type="text" placeholder="Enter your password" />
             <button type="submit">
               <strong>Login</strong>
             </button>
           </form>
-          <button
-            onClick={() => {
-              navigate('/findid');
-            }}
-            id="find_id_button"
-          >
-            아이디 찾기
-          </button>
-          <button id="find_pw_button">비밀번호 찾기</button>
-          <button id="sign_in">회원가입</button>
         </div>
       </IDBackground>
+      <div className={styles.FindnSign}>
+        <button
+          onClick={() => {
+            navigate('/findid');
+          }}
+          id="find_id_button"
+        >
+          아이디 찾기
+        </button>
+        <button id="find_pw_button">비밀번호 찾기</button>
+        <button id="sign_in">회원가입</button>
+      </div>
     </div>
   );
 }
