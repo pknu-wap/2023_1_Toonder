215def23927e79761cbb83c073af218c3c78bf0b
kitewatermelon <80404308+kitewatermelon@users.noreply.github.com>
2023-04-13 08:49:58 +0900

    feat/TO-27: 1.login창과 findid 창을 css적으로 구분했습니다. / 2. login창을 css적으로 수정했습니다. / 3. '구현 필요'와 '설명'이라는 주석을 추가했습니다.

--- toonder/src/component/findid.js
+++ toonder/src/component/findid.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import IDBackground from './ID_background';
-import Findid_after from './findid_after';
 import { useNavigate } from 'react-router-dom';
 import "../style.css";
 
@@ -11,8 +10,10 @@ function Findid() {
     <IDBackground text="Find ID">
         <div className="findid_body">
           <form>
+            {/*설명: email과 이름을 넣는 입력창*/}
             <input id="enter_name" type="text" placeholder="Enter your name" />
-            <input id="enter_id" type="text" placeholder="Enter your ID" />
+            <input id="enter_id" type="text" placeholder="Enter your E-mail" />
+            {/*구현 필요: DB와 연동하여 find 버튼응 눌렀을때 아이디를 알려주는 기능 필요*/}
             <button
             onClick={() => {
               navigate('/findid_after');
