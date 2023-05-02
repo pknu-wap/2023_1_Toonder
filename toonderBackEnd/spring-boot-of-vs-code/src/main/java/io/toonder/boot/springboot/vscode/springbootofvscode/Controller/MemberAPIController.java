5e3bcf0c5a600a68aef58f8129a3749d736ac488
wkdghdnws199 <wkdghdwns199@naver.com>
2023-05-03 04:03:38 +0900

    Refactor : Changed route to /api/member

--- toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/Controller/MemberAPIController.java
+++ toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/Controller/MemberAPIController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
  
-@RequestMapping("/member")
+@RequestMapping("/api/member")
 @RestController
 public class MemberAPIController {
     @Autowired
