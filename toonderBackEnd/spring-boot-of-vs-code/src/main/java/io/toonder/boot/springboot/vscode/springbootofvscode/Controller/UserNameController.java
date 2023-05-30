8449294f487ed02f16e00ddb4293246bdba3be11
JH <wngml9720@naver.com>
2023-05-30 22:37:48 +0900

    수정

--- toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/Controller/UserNameController.java
+++ toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/Controller/UserNameController.java
@@ -1,15 +1,33 @@
 package io.toonder.boot.springboot.vscode.springbootofvscode.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import io.toonder.boot.springboot.vscode.springbootofvscode.Member;
+import io.toonder.boot.springboot.vscode.springbootofvscode.MemberRepository;
+
+@RequestMapping("/api/member")
 @RestController
 public class UserNameController {
-
-    @GetMapping("/api/username")
-    public String test() {
-        return "장홍준";
-
-        // 대충 DB에서 가져온 데이터를 보내줍니당
+    @Autowired
+    private MemberRepository memberRepository;
+    
+    @RequestMapping(value = "/select/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+    public Map<String, String> selectById(@PathVariable String id) {
+        Member member = memberRepository.findById(id).orElse(null);
+        if (member == null) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        result.put("mem_name", member.getMem_name());
+        result.put("mem_hashtag", member.getMem_hashtag());
+        return result;
     }
 }
