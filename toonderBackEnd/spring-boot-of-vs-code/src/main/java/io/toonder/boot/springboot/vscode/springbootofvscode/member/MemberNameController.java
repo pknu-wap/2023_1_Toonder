356cef013c5baf978b7c06c9b46d986e9de94470
JangHongJoon <wkdghdwns19969@gmail.com>
2023-06-02 03:25:16 +0900

    feat : MemberNameController API added

--- toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/member/MemberNameController.java
+++ toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/member/MemberNameController.java
@@ -3,28 +3,33 @@ package io.toonder.boot.springboot.vscode.springbootofvscode.member;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@RequestMapping("/api/member")
+@RequestMapping("/toonder")
 @RestController
-public class UserNameController {
+public class MemberNameController {
     @Autowired
     private MemberRepository memberRepository;
     
-    @RequestMapping(value = "/select/{id}", method = {RequestMethod.GET, RequestMethod.POST})
-    public Map<String, String> selectById(@PathVariable String id) {
-        Member member = memberRepository.findById(id).orElse(null);
+    @RequestMapping(value = "/name", method = {RequestMethod.GET, RequestMethod.POST})
+    public Map<String, String> selectById(@RequestBody Map<String,String> memberEmailJsonString){
+
+        Member member = memberRepository.findById(memberEmailJsonString.get("email")).orElse(null);
         if (member == null) {
             return null;
         }
         Map<String, String> result = new HashMap<>();
         result.put("mem_name", member.getMem_name());
-        result.put("mem_hashtag", member.getMem_hashtag());
         return result;
     }
 }
