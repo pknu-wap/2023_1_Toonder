e5f6417612aad87821b4a261cd5ec73c0ea81b39
Lee SeungYeop <dltmdduq8484@gmail.com>
2023-06-02 16:25:40 +0900

    1.컴포넌트 업데이트반영이 안되어있는것들을 반영시킴 2.npm start가 안되서 에러 고침 3.중복되거나 다른 컴포넌트지만 기능이 중복되는 컴포넌트는 삭제

--- toonder/src/App.js
+++ toonder/src/App.js
@@ -7,7 +7,6 @@ import Findpw from './component/find/findpw';
 import Login from './component/login/login';
 import Mainpage from './component/main/main_page';
 import Findid_after from './component/find/findid_after';
-import Findpw_after from './component/find/findpw_after';
 import MainWebtoonList from './component/main/mainWebtoonList';
 import MainBacksmall from './component/backgrounds/mainBackSmall';
 import MainWebtoonInfo from './component/main/mainWebtoonInfo';
@@ -15,9 +14,7 @@ import Mypage from './component/main/mypage';
 import Freeboard from './component/freeboard/freeboard';
 import InfoC from './component/main/infoChange';
 import axios from 'axios';
-import Profilechange from './component/main/profilechange'
-import InfoChange from        './component/main/infoChange'
-import CheckPassword from      './component/find/check';
+import Profilechange from './component/main/profilechange';
 
 function App() {
   //Test for Spring Data setting
@@ -41,20 +38,16 @@ function App() {
           <Route path="/main_page" element={<Mainpage />} />
           <Route path="/findid" element={<Findid />} />
           <Route path="/findpw" element={<Findpw />} />
-          <Route path="/check_password" element={<CheckPassword />} />
           <Route path="/findid_after" element={<Findid_after />} />
-          <Route path="/findpw_after" element={<Findpw_after />} />
           <Route path="/mainWebtoonList" element={<MainWebtoonList />} />
           <Route path="/mainbackground" element={<MainBacksmall />} />
           <Route path="/mainwebtooninfo" element={<MainWebtoonInfo />} />
-          <Route path="/info_change" element={<InfoChange />} />
           <Route path="/freeboard" element={<Freeboard />} />
-          <Route path="/profilechange" element={<Profilechange />} />
           <Route path="/mypage" element={<Mypage />} />
           <Route path="/infochange" element={<InfoC />} />
           <Route path="/newpw" element={<Newpw />} />
           <Route path="/profilechange" element={<Profilechange />} />
-          <Route path='/join' element={<Join/>}/>
+          <Route path="/join" element={<Join />} />
         </Routes>
       </BrowserRouter>
     </div>
--- toonder/src/component/backgrounds/mainBackSmall.js
+++ toonder/src/component/backgrounds/mainBackSmall.js
@@ -1,6 +1,6 @@
-import React,{useState, useEffect} from 'react';
-import { useNavigate , Link} from 'react-router-dom';
-import "./backGround.module.css";
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './backGround.module.css';
 import ex1 from '../../images/ex1.png';
 import axios from 'axios';
 
@@ -9,53 +9,62 @@ function MainBackSmall(props) {
   const [loggedUserName, setLoggedUserName] = useState('지금 로그인하세요!');
   useEffect(() => {
     axios
-      .get('api/member/select/'+sessionStorage.getItem('loggedUserEmail'))
-      .then(loggedUserData => {
+      .get('api/member/select/' + sessionStorage.getItem('loggedUserEmail'))
+      .then((loggedUserData) => {
         setLoggedUserName(loggedUserData.data.mem_name);
       })
-      .catch(error => console.log(error))
-  },[])
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <div className='mainBackSmall'>
-        <Link to='/mypage'>
-          <div className='mainInfo'>
-            <Link to='/profilechange'>
-              <img id='infoimg' src={ex1} alt="image error"></img>
-            </Link>
-            <h2>{loggedUserName}</h2>
-            <button id='changeInfo'
-            onClick={() => {
-              navigate('/check_password');
-            }}
-            ><h3>정보수정</h3></button>
-          </div>
-        </Link>
-
-        <div className='mainButtonSet'>
-        <button id="webtoonList"
-        onClick={() => {
-          navigate('/mainWebtoonList');
-        }}
-        >웹툰 목록</button>
-        <button id="mypage"
+    <div className="mainBackSmall">
+      <Link to="/infochange">
+        <div className="mainInfo">
+          <Link to="/profilechange">
+            <img id="infoimg" src={ex1} alt="image error"></img>
+          </Link>
+          <h2>{loggedUserName}</h2>
+          <button id="changeInfo">
+            <h3>정보수정</h3>
+          </button>
+        </div>
+      </Link>
+      <div className="mainButtonSet">
+        <button
+          id="webtoonList"
+          onClick={() => {
+            navigate('/mainWebtoonList');
+          }}
+        >
+          웹툰 목록
+        </button>
+        <button
+          id="mypage"
           onClick={() => {
             navigate('/mypage');
           }}
-        >마이페이지</button>
-        <button id="freeBoard"
+        >
+          마이페이지
+        </button>
+        <button
+          id="freeBoard"
           onClick={() => {
             navigate('/freeboard');
           }}
-        >자유게시판</button>
-        <button id="logOut"
-        onClick={() => {
-          navigate('/');
-        }}
-        >로그아웃</button>{/*로그아웃 시 세션 만료했음을 나타내는 기능 필요함 */}
+        >
+          자유게시판
+        </button>
+        <button
+          id="logOut"
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          로그아웃
+        </button>
+        {/*로그아웃 시 세션 만료했음을 나타내는 기능 필요함 */}
       </div>
       {props.children} {/* props.children 렌더링 */}
-
     </div>
   );
 }
