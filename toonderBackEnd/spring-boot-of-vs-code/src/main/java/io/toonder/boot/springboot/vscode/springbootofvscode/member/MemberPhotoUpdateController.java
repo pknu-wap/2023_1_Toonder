fb86d6981f386ca73dec62e5ad9babc5988899dc
JangHongJoon <wkdghdwns19969@gmail.com>
2023-06-04 17:53:46 +0900

    feat : Added for member photo update api function

---
+++ toonderBackEnd/spring-boot-of-vs-code/src/main/java/io/toonder/boot/springboot/vscode/springbootofvscode/member/MemberPhotoUpdateController.java
@@ -0,0 +1,32 @@
+package io.toonder.boot.springboot.vscode.springbootofvscode.member;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/toonder")
+@RestController
+public class MemberPhotoUpdateController {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @RequestMapping(value = "/photo/update", method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json; charset=utf-8")
+    public Member updatePhoto(@RequestBody Map<String, String> memberEmailAndImageFile){
+
+        System.out.println("------------------------");
+        System.out.println("HI");
+        System.out.println(memberEmailAndImageFile.get("email"));
+        System.out.println(memberEmailAndImageFile.get("image").replace("data:",""));
+        System.out.println("------------------------");
+
+        Member member = memberRepository.findById(memberEmailAndImageFile.get("email")).orElse(null);
+        member.setMem_photo(memberEmailAndImageFile.get("image").replace("data:",""));
+
+        return memberRepository.save(member);
+    }   
+}
