b59e5f690573b0f054ec329ea37aebef288f7dd5
JangHongJoon <wkdghdwns19969@gmail.com>
2023-06-09 18:04:38 +0900

    refactor : Redirec

--- toonder/src/component/find/findpw.js
+++ toonder/src/component/find/findpw.js
@@ -40,7 +40,7 @@ function Findpw() {
     setMessage('');
     try {
       await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'http://localhost:3000/newpw',
+        redirectTo: 'http://ec2-15-164-55-83.ap-northeast-2.compute.amazonaws.com/newpw',
       });
       const [id, domain] = email.split('@');
       const hiddenID = id.slice(0, 3) + id.slice(3).replace(/./g, '*');
